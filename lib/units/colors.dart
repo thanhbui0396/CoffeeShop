4a03d1c86c4b3340ff6a2bcd1ae81d7ae5bd693a
thanh bui (strigram) <thanhbui96@odoo.com>
2023-07-15 16:31:57 +0700

    Create new page regis, update loginPage,config color and dimensions, import get.dart

---
+++ lib/units/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const mainColor = Color(0xFFEDAB54);
+  static const textAndButtonColor = Color(0xFFE98024);
+  static const buttonColor2 = Color(0xFF8E4D00);
+  static const voucherColor = Color(0xFFFFEBDA);
+}
